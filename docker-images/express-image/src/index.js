b09e112aa94d2d87d359a59bc5907b2ea0d20df1
antoine.rochat <ant.rochat@hotmail.fr>
2018-06-10 16:18:01 +0200

    Added a docker image wrapping an express.js that returns random arrays of animals

---
+++ docker-images/express-image/src/index.js
@@ -0,0 +1,37 @@
+var Chance = require('chance');
+var chance = new Chance();
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send(generateAnimals());
+})
+
+app.listen(3000, function () {
+  console.log('Accepting HTTP requests on port 3000.')
+})
+
+function generateAnimals() {
+	var numberOfAnimals = chance.integer({
+		min: 0,
+		max: 10
+	});
+	console.log(numberOfAnimals);
+	var animals = [];
+	for(var i = 0; i < numberOfAnimals; i++){
+		var birthYear = chance.year({
+			min: 1986,
+			max: 1996
+		});
+		animals.push({
+			name: chance.first(),
+			animalType: chance.animal(),
+			birthday: chance.birthday({
+				year: birthYear
+			})
+		});
+	};
+	console.log(animals);
+	return animals;
+}
